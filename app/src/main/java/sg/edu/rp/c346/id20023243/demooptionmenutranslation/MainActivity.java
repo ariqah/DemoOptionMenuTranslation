2025da9395cedf2a48fcde609ac43ea99f1985c8
ariqah <20023243@rp.edu.sg>
2021-06-02 20:33:09 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/id20023243/demooptionmenutranslation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20023243/demooptionmenutranslation/MainActivity.java
@@ -1,14 +1,43 @@
 package sg.edu.rp.c346.id20023243.demooptionmenutranslation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText, tvTranslatedText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+        tvTranslatedText2 = findViewById(R.id.textViewTranslatedText2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu); // expand menu from xml to this menu; otherwise menu empty
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //for this if u change language to italian; all contents will be changed to italian
+        if(item.getItemId()== R.id.englishSelection) {
+            tvTranslatedText.setText("Hello");
+            tvTranslatedText2.setText("Bye");
+        }
+        else if(item.getItemId()== R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            tvTranslatedText2.setText("Addio");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
